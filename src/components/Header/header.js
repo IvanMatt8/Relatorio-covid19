ad0cebc727d0452cc36550f24896f1ef7b9d1beb
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-22 11:11:19 -0300

    :sparkles: criado novo componente Header

---
+++ src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+function Header() {
+  return (
+    <nav>
+      <h2>RELATORIOS COVID-19</h2>
+      <h2>CASOS</h2>
+    </nav>
+  );
+}
+
+export default Header;
