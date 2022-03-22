fa7213205556114d4071b04a581212eb3288817d
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-22 11:15:37 -0300

    :rocket: import componente Covid no App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Covid from "./pages/index";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Covid />
+    </div>
+  );
 }
 
 export default App;
